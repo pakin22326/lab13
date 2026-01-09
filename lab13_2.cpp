7875e5f16d52ed1a2b7e659c7275a341c0d5b80f
pakin22326 <pakinounruen@gmail.com>
2026-01-09 17:07:06 +0700

    completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M], int s, int x, int y)
+{
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if( sqrt((i - x)*(i - x) + (j - y)*(j - y)) <= s - 1 ){
+                image[i][j] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][M])
+{
+     for(int j = 0; j < M + 2; j++)
+        cout << "-";
+    cout << endl;
+
+    for(int i = 0; i < N; i++){
+        cout << "|";
+        for(int j = 0; j < M; j++){
+            if(image[i][j])
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "|" << endl;
+    }
+
+     for(int j = 0; j < M + 2; j++)
+        cout << "-";
+    cout << endl;
+}
